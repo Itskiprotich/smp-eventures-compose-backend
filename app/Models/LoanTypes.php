26cd804e1cd516fd9d9761767fb76d739461bfb7
itskiprotich <keeprawteachjapheth@gmail.com>
2024-09-19 13:40:56 +0300

    UPD: withdrawals

--- app/Models/LoanTypes.php
+++ app/Models/LoanTypes.php
@@ -18,4 +18,15 @@ class LoanTypes extends Model
         'interest_rate'
 
     ];
+      // Custom accessor for created_at in a specific format
+      public function getCreatedAtFormattedAttribute()
+      {
+          return $this->created_at->format('Y-m-d H:i:s'); // Adjust format as needed
+      }
+  
+      // Custom accessor for updated_at in a specific format
+      public function getUpdatedAtFormattedAttribute()
+      {
+          return $this->updated_at->format('Y-m-d H:i:s'); // Adjust format as needed
+      }
 }
--- app/Models/Withdrawals.php
+++ app/Models/Withdrawals.php
@@ -13,4 +13,15 @@ class Withdrawals extends Model
         'reference',
         'action_by', 'approved_by', 'amount', 'balance', 'narration', 'mode', 'status'
     ];
+      // Custom accessor for created_at in a specific format
+      public function getCreatedAtFormattedAttribute()
+      {
+          return $this->created_at->format('Y-m-d H:i:s'); // Adjust format as needed
+      }
+  
+      // Custom accessor for updated_at in a specific format
+      public function getUpdatedAtFormattedAttribute()
+      {
+          return $this->updated_at->format('Y-m-d H:i:s'); // Adjust format as needed
+      }
 }
